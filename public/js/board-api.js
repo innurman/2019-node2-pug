722c1c8e6cbdeff70401ef20741937d74f20e701
innurman <innurman@gmail.com>
2020-01-12 16:30:24 +0900

    Added write() and update() from api.js

--- public/js/board-api.js
+++ public/js/board-api.js
@@ -1,13 +1,64 @@
-$("#bt").click(function(){
+$("#btSave").click(function(){
+    var id = document.form1.id.value;
+    var type = "POST";
+    var url = "/api/post";
+    if(id != "") {
+        type = "PUT";
+        url = "/api/put";
+    }
     $.ajax({
-        url: "/api/get",
-        type: "GET",
+        url: "/api/post",
+        type: "POST",
         dataType: "json",
-        success: function(res) {
-            console.log(res);
+        data: {
+            id: id,
+            title: document.form1.title.value,
+            content: document.form1.content.value,
+            writer: document.form1.writer.value,
         },
-        error: function(xhr, status, error) {
+        success: function(res) {
+            document.form1.reset();
+            if(res.serverStatus == 2) getData();
+        },
+        error: function(xhr) {
             console.log(xhr);
         }
     });
 });
+
+function getData(id) {
+    if(id) url = "/api/get/"+id;
+    else url = "/api/get/";
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function(res) {
+            if(id) viewList(res);
+            else makeList(res);
+        },
+        error: function(xhr) {
+            console.log(xhr);
+        }
+    });
+}
+function makeList(res) {
+    console.log(res);
+    $("#title").html(res.title);
+    var html = '';
+    for(var i in res.data) {
+        html += '<tr>';
+        html += '<td>'+res.data[i].id+'</td>';
+        html += '<td>'+res.data[i].title+'</td>';
+        html += '<td>'+res.data[i].writer+'</td>';
+        html += '<td>'+res.data[i].wdate+'</td>';
+        html += '<td>'+res.data[i].rnum+'</td>';
+        html += '<td><button class="btn btn-sm btn-danger" data-id="'+res.data[i].id+'">삭제</button></td>';
+        html += '</tr>';
+    }
+    $("#listTb > tbody").html(html);
+}
+function viewList(res) {
+    console.log(res);
+}
+getData();
--- router/api.js
+++ router/api.js
@@ -10,8 +10,12 @@ delete : Delete/Delete
 */
 
 // RESTFUL API
+// http://127.0.0.1:3000/api.html
 router.get(["/get", "/get/:id"], async(req, res) => {
     let sql = '';
+    const vals = {
+        title: "API 게시판"
+    };
     if(req.params.id) {
         sql = "SELECT * FROM board WHERE id="+req.params.id;
     }
@@ -22,11 +26,25 @@ router.get(["/get", "/get/:id"], async(req, res) => {
     
     const connect = await pool.getConnection();
     const result = await connect.query(sql);
+    vals.data  = result[0];
+    connect.release();
+    res.json(vals);
+});
+
+// write and update from board-api.js
+router.post("/post", async (req, res) => {
+    let title = req.body.title;
+    let content = req.body.content;
+    let writer = req.body.writer;
+    let wdate = new Date();
+    let sql = "INSERT INTO board SET title=?, content=?, writer=?, wdate=?";
+    let sqlVals = [title, content, writer, wdate];
+    
+    let connect = await pool.getConnection();
+    let result = await connect.query(sql, sqlVals);
     connect.release();
     res.json(result[0]);
 });
-
-//router.post();
 
 // method-override
 // https://www.npmjs.com/package/method-override
